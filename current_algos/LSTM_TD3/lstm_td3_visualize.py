1b36864c6aa8b27f35b6cd82c6df54145ac761cf
MarWaltz <martin.waltz@tu-dresden.de>
2022-01-03 09:09:35 +0100

    change lstm_td3 visualization

--- current_algos/LSTM_TD3/lstm_td3_visualize.py
+++ current_algos/LSTM_TD3/lstm_td3_visualize.py
@@ -1,35 +1,90 @@
+import argparse
+import copy
+import pickle
+import random
+import sys
+import time
+
 import gym
 import numpy as np
-#import pybulletgym
+import pybulletgym
 import torch
-#from current_algos.common.POMDP_wrapper import POMDP_Wrapper
+from common.eval_plot import plot_from_progress
+from configs.continuous_actions import __path__
 from current_algos.LSTM_TD3.lstm_td3_agent import *
 from current_envs.envs import *
-from current_envs.wrappers import MinAtari_wrapper, gym_POMDP_wrapper
-
-# training config
-EVAL_EPISODES = 10      # number of episodes to average per evaluation
+from current_envs.wrappers.gym_POMDP_wrapper import gym_POMDP_wrapper
 
 
-def visualize_policy(env_str, POMDP_type, actor_weights, critic_weights):
+
+def visualize_policy(c, agent_name, actor_weights, critic_weights):
     
-    if env_str == "LCP":
-        test_env = ObstacleAvoidance(POMDP_type=POMDP_type)
+    # measure computation time
+    start_time = time.time()
     
-    elif env_str == "Ski":
-        test_env = Ski(POMDP_type=POMDP_type)
+    # init envs
+    env = gym.make(c["env"]["name"], **c["env"]["env_kwargs"])
+    test_env = gym.make(c["env"]["name"], **c["env"]["env_kwargs"])
 
-    test_agent = LSTM_TD3_Agent(mode      = "test",
-                      action_dim     = test_env.action_space.shape[0], 
-                      obs_dim      = test_env.observation_space.shape[0], 
-                      action_high    = test_env.action_space.high[0],
-                      action_low     = test_env.action_space.low[0], 
-                      actor_weights  = actor_weights, 
-                      critic_weights = critic_weights)
+    # wrappers
+    for wrapper in c["env"]["wrappers"]:
+        wrapper_kwargs = c["env"]["wrapper_kwargs"][wrapper]
+        env = eval(wrapper)(env, **wrapper_kwargs)
+        test_env = eval(wrapper)(test_env, **wrapper_kwargs)
+    
+    # get state_shape
+    if c["env"]["state_type"] == "image":
+        raise NotImplementedError("Currently, image input is not available for continuous action spaces.")
+    
+    elif c["env"]["state_type"] == "feature":
+        obs_dim = env.observation_space.shape[0]
+
+    # seeding
+    print(c["seed"])
+    env.seed(c["seed"])
+    test_env.seed(c["seed"])
+    torch.manual_seed(c["seed"])
+    np.random.seed(c["seed"])
+    random.seed(c["seed"])
+
+    # init agent
+    test_agent = LSTM_TD3_Agent(mode             = "test",
+                           action_dim       = env.action_space.shape[0], 
+                           action_high      = env.action_space.high[0],
+                           action_low       = env.action_space.low[0],
+                           obs_dim          = obs_dim,
+                           actor_weights    = actor_weights,
+                           critic_weights   = critic_weights, 
+                           input_norm       = c["input_norm"],
+                           input_norm_prior = c["input_norm_prior"],
+                           double_critic    = c["agent"][agent_name]["double_critic"],
+                           tgt_pol_smooth   = c["agent"][agent_name]["tgt_pol_smooth"],
+                           tgt_noise        = c["tgt_noise"],
+                           tgt_noise_clip   = c["tgt_noise_clip"],
+                           pol_upd_delay    = c["agent"][agent_name]["pol_upd_delay"],
+                           gamma            = c["gamma"],
+                           tau              = c["tau"],
+                           net_struc_actor  = c["net_struc_actor"],
+                           net_struc_critic = c["net_struc_critic"],
+                           lr_actor         = c["lr_actor"],
+                           lr_critic        = c["lr_critic"],
+                           buffer_length    = c["buffer_length"],
+                           grad_clip        = c["grad_clip"],
+                           grad_rescale     = c["grad_rescale"],
+                           act_start_step   = c["act_start_step"],
+                           upd_start_step   = c["upd_start_step"],
+                           upd_every        = c["upd_every"],
+                           batch_size       = c["batch_size"],
+                           history_length   = c["history_length"],
+                           use_past_actions = c["use_past_actions"],
+                           device           = c["device"],
+                           env_str          = c["env"]["name"],
+                           info             = c["env"]["info"],
+                           seed             = c["seed"])
 
     rets = []
     
-    for _ in range(EVAL_EPISODES):
+    for _ in range(c["eval_episodes"]):
 
         # init history
         o_hist = np.zeros((test_agent.history_length, test_agent.obs_dim))
@@ -42,11 +97,12 @@ def visualize_policy(env_str, POMDP_type, actor_weights, critic_weights):
         # potentially normalize it
         if test_agent.input_norm:
             o = test_agent.inp_normalizer.normalize(o, mode="test")
-        cur_ret = 0
 
+        cur_ret = 0
         d = False
         
         while not d:
+
             # render
             test_env.render(agent_name=test_agent.name)
 
@@ -84,9 +140,34 @@ def visualize_policy(env_str, POMDP_type, actor_weights, critic_weights):
 
 if __name__ == "__main__":
     
+    # get config and name of agent
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file", type=str, default="oa_lstm_td3_mdp.json")
+    parser.add_argument("--agent_name", type=str, default="lstm_td3")
+    parser.add_argument("--seed", type=int, default=random.randint(0, 100000))
+    args = parser.parse_args()
+
+    # read config file
+    with open(__path__._path[0] + "/" + args.config_file) as f:
+        c = json.load(f)
+
+    # potentially overwrite seed
+    if args.seed is not None:
+        c["seed"] = args.seed
+
+    # convert certain keys in integers
+    for key in ["seed", "timesteps", "epoch_length", "eval_episodes", "buffer_length", "act_start_step",\
+         "upd_start_step", "upd_every", "batch_size", "history_length"]:
+        c[key] = int(c[key])
+
+    # handle maximum episode steps
+    if c["env"]["max_episode_steps"] == -1:
+        c["env"]["max_episode_steps"] = np.inf
+    else:
+        c["env"]["max_episode_steps"] = int(c["env"]["max_episode_steps"])
+
     # set number of torch threads
     torch.set_num_threads(torch.get_num_threads())
 
     # run main loop
-    visualize_policy(env_str="Ski", POMDP_type="MDP", critic_weights="LSTM_TD3_Agent_critic_weights.pth", 
-                     actor_weights="LSTM_TD3_Agent_actor_weights.pth")
+    visualize_policy(c, args.agent_name, actor_weights="lstm_td3_agent_actor_weights.pth", critic_weights="lstm_td3_agent_critic_weights.pth")
