3f02cc51e404a729cb24f7f7a34902e0441832b8
MarWaltz <mwz@posteo.de>
2023-04-26 12:23:16 +0200

    remove MPI from logging_func

--- tud_rl/common/logging_func.py
+++ tud_rl/common/logging_func.py
@@ -9,51 +9,12 @@ import os.path as osp
 from datetime import date
 
 import numpy as np
-from mpi4py import MPI
 
 
-def allreduce(*args, **kwargs):
-    return MPI.COMM_WORLD.Allreduce(*args, **kwargs)
-
-def num_procs():
-    """Count active MPI processes."""
-    return MPI.COMM_WORLD.Get_size()
-
-def mpi_op(x, op):
-    x, scalar = ([x], True) if np.isscalar(x) else (x, False)
-    x = np.asarray(x, dtype=np.float32)
-    buff = np.zeros_like(x, dtype=np.float32)
-    allreduce(x, buff, op=op)
-    return buff[0] if scalar else buff
-
-def mpi_sum(x):
-    return mpi_op(x, MPI.SUM)
-
-def mpi_avg(x):
-    """Average a scalar or vector over MPI processes."""
-    return mpi_sum(x) / num_procs()
-
-def mpi_statistics_scalar(x, with_min_and_max=False):
-    """
-    Get mean/std and optional min/max of scalar x across MPI processes.
-    Args:
-        x: An array containing samples of the scalar to produce statistics
-            for.
-        with_min_and_max (bool): If true, return min and max of x in 
-            addition to mean and std.
-    """
-    x = np.array(x, dtype=np.float32)
-    global_sum, global_n = mpi_sum([np.sum(x), len(x)])
-    mean = global_sum / global_n
-
-    global_sum_sq = mpi_sum(np.sum((x - mean)**2))
-    std = np.sqrt(global_sum_sq / global_n)  # compute global std
-
+def statistics(x, with_min_and_max=False):
     if with_min_and_max:
-        global_min = mpi_op(np.min(x) if len(x) > 0 else np.inf, op=MPI.MIN)
-        global_max = mpi_op(np.max(x) if len(x) > 0 else -np.inf, op=MPI.MAX)
-        return mean, std, global_min, global_max
-    return mean, std
+        return np.mean(x), np.std(x), np.min(x), np.max(x)
+    return np.mean(x), np.std(x)
 
 def is_json_serializable(v):
     try:
@@ -263,8 +224,7 @@ class EpochLogger(Logger):
             v = self.epoch_dict[key]
             vals = np.concatenate(v) if isinstance(
                 v[0], np.ndarray) and len(v[0].shape) > 0 else v
-            stats = mpi_statistics_scalar(
-                vals, with_min_and_max=with_min_and_max)
+            stats = statistics(vals, with_min_and_max=with_min_and_max)
             super().log_tabular(
                 key if average_only else 'Avg_' + key, stats[0])
             if not(average_only):
